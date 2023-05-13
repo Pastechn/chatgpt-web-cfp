e8c3399fa8f342bb9b6e510aa20458e6e85fb1fc
Pastechn <62227537+Pastechn@users.noreply.github.com>
2023-05-13 10:28:50 +0800

    Update cf-pages-build.sh

--- cf-pages-build.sh
+++ cf-pages-build.sh
@@ -5,6 +5,7 @@ sed -i 's|;;API_URL;;|'${API_URL}'|g' .env
 sed -i 's|;;BASE_URL_WITH_SLASH;;|'${BASE_URL_WITH_SLASH}'|g' .env
 sed -i 's|;;ENABLE_LONG_REPLY;;|'${ENABLE_LONG_REPLY}'|g' .env
 sed -i 's|;;ENABLE_PWA;;|'${ENABLE_PWA}'|g' .env
+cat .env
 
 # Build frontend
 npm install -g pnpm
